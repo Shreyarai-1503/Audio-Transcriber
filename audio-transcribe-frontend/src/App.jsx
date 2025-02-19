cd547fd7d9d5eeed99b04767baa3ca3f8c78ce56
Shreya <shreyarai1503@gmail.com>
2025-02-19 12:50:57 +0530

    transcriber frontend and connection with backend

---
+++ audio-transcribe-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AudioUploader from './components/AudioUploader'
+
+const App = () => {
+  return (
+    <>
+      <AudioUploader/>
+    </>
+  )
+}
+
+export default App
